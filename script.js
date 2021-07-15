7eefe5b00ade45e03e2f4c64127f40f42853f57c
iainaitken <iain_aitken@yahoo.co.uk>
2021-07-15 15:00:27 +0100

    Add turn checker to roll event listener

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const newGame = document.querySelector('.btn--new');
 let p1Score = defaultScore;
 const p1ScoreDOM = document.querySelector('#score--0');
 const roll = document.querySelector('.btn--roll');
+let turn = 'p1';
 
 // Event Listeners
 
 roll.addEventListener('click', function () {
   const number = randomNumber();
-  p1Score += number;
+  if (turn === 'p1') {
+    p1Score += number;
+  }
   console.log(p1Score);
 });
 
