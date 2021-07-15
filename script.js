194c981d86807c095630e4ab7dbb9cf96c8d3071
iainaitken <iain_aitken@yahoo.co.uk>
2021-07-15 14:50:00 +0100

    Amend prettier settings; add random number generator on click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const newGame = document.querySelector('.btn--new');
+const roll = document.querySelector('.btn--roll');
+
+roll.addEventListener('click', function () {
+  console.log(Math.ceil(Math.random() * 6));
+});
+
+console.log(roll);
