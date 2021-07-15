537174e6181be655cef5bc679dee24603b971f28
iainaitken <iain_aitken@yahoo.co.uk>
2021-07-15 15:52:00 +0100

    Add feature - score updates if hold button is pressed

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 // DOM objects
 
+const hold = document.querySelector('.btn--hold');
 const newGame = document.querySelector('.btn--new');
 const p1CurrentScore = document.querySelector('#current--0');
 const p2CurrentScore = document.querySelector('#current--1');
 const p1ScoreDOM = document.querySelector('#score--0');
+const p2ScoreDOM = document.querySelector('#score--1');
 const roll = document.querySelector('.btn--roll');
 
 // Variables
@@ -13,9 +15,15 @@ const roll = document.querySelector('.btn--roll');
 const defaultScore = 0;
 let currentScore = defaultScore;
 let p1Score = defaultScore;
+let p2Score = defaultScore;
 let turn = 'p1';
 
 // Event Listeners
+
+hold.addEventListener('click', function () {
+  updateScore();
+  endTurn();
+});
 
 roll.addEventListener('click', function () {
   const number = randomNumber();
@@ -29,20 +37,26 @@ roll.addEventListener('click', function () {
 
 // Functions
 
-function randomNumber() {
-  return Math.ceil(Math.random() * 6);
-}
-
 function changePlayer() {
   turn = turn === 'p1' ? 'p2' : 'p1';
 }
 
-function updateCurrentScore(number) {
-  currentScore += number;
-  updateDOMScore();
+function endTurn() {
+  currentScore = defaultScore;
+  updateDOMCurrentScore();
+  changePlayer();
 }
 
-function updateDOMScore() {
+function randomNumber() {
+  return Math.ceil(Math.random() * 6);
+}
+
+function updateCurrentScore(number) {
+  currentScore += number;
+  updateDOMCurrentScore();
+}
+
+function updateDOMCurrentScore() {
   if (turn === 'p1') {
     p1CurrentScore.textContent = currentScore;
   } else {
@@ -50,8 +64,12 @@ function updateDOMScore() {
   }
 }
 
-function endTurn() {
-  currentScore = defaultScore;
-  updateDOMScore();
-  changePlayer();
+function updateScore() {
+  if (turn === 'p1') {
+    p1Score += currentScore;
+    p1ScoreDOM.textContent = p1Score;
+  } else {
+    p2Score += currentScore;
+    p2ScoreDOM.textContent = p2Score;
+  }
 }
